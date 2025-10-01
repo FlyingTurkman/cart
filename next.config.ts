3505f0cdc5b3b81bcfb82d434b7a559480567391
Sharkman <dytdenizarli@hotmail.com>
2025-10-01 14:30:31 +0300

    ui components added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: '/cart-static',
+  basePath: '/cart'
 };
 
 export default nextConfig;
