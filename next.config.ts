a353cbd54ab9989ee40918a8635f94c9690a7c14
Sharkman <dytdenizarli@hotmail.com>
2025-10-01 18:37:52 +0300

    cors updated
    cors update for home

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   assetPrefix: '/cart-static',
-  basePath: '/cart'
+  basePath: '/cart',
+  async headers() {
+    return [
+      {
+        source: '/api/homeApi/:path*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Token" }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
